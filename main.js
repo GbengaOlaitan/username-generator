eec7dbc36391521f0b69f49dcfc5a4fc55c0ed47
Ibrahim <98415538+AdebayoIbrahim@users.noreply.github.com>
2025-06-09 17:55:02 +0100

    Update main.js
    Fixed DRY code

--- main.js
+++ main.js
@@ -12,33 +12,26 @@ console.log(isUserNameAccepted);
 
 //Make Buttton Generate Username When clicked;
 
-function generateRandomFirstNameNumber(firstName) {
-  const firstNameArr = [];
+function generateRandomNameNumber(nameParams) {
+  const nameArraygenerated = [];
   for (let i = 0; i < 3; i++) {
-    const firstNameLength = Math.floor(Math.random() * firstName.length);
-    firstNameArr.push(firstName.charAt(firstNameLength));
+    //math.random returns floating point between 0 t0 0.9 
+    //also multiplying by the name length makes sure index doesn't exceed limit i.e 
+    //if length is 5 random number generated is always less then 5 whichis safe coz array indexing starts at 0 to length -1
+    const namePramsIndex = Math.floor(Math.random() * nameParams.length);
+    nameArraygenerated.push(nameParams.charAt(namePramsIndex));
   }
 
-  return firstNameArr;
+  return nameArraygenerated;
 }
 
-//function generating lastName array
-function generateRandomLastNameNumber(lastName) {
-  const lastNameArr = [];
-  for (let i = 0; i < 3; i++) {
-    const lastNameLength = Math.floor(Math.random() * lastName.length);
-    lastNameArr.push(lastName.charAt(lastNameLength));
-  }
-
-  return lastNameArr;
-}
 
 generateButton.addEventListener("click", () => {
   if (firstName.value !== "" && lastName.value !== "") {
     const firstNameElement = firstName.value.toLowerCase().trim();
     const lastNameElement = lastName.value.toLowerCase().trim();
-    const generatedfirstName = generateRandomFirstNameNumber(firstNameElement);
-    const generatedLastName = generateRandomLastNameNumber(lastNameElement);
+    const generatedfirstName = generateRandomNameNumber(firstNameElement);
+    const generatedLastName = generateRandomNameNumber(lastNameElement);
     const userNameArr = [...generatedfirstName, ...generatedLastName];
     userNameGenerated = `@${userNameArr.join("")}`;
     userName.value = userNameGenerated;
